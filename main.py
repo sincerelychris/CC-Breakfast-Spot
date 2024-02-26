696532a6369b05aee23ef3a0fdad1041d0303548
Chris <96923618+sincerelychris@users.noreply.github.com>
2024-02-26 11:11:15 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+
+#if you walk in workers and get name:
+name = input("Hi, Welcome to the 5 Star Breakfast! Whats your name.")
+
+#Apply name:
+print("Hi "+name+ ". It is nice to meet you")
+
+menu = "Pancakes, Cheese Toast, Waffles"
+
+print(name + ", what would you like from our menu today: \n"+menu+"?")
+order = input()
+
+print("Great Choice "+name+". Your " +order+ " will be finished in 2 minutes")
+
